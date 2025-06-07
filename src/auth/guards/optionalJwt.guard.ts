e8a106d3c1759ad2aa2bd2ae0273bf540ddf3ed6
fabiangzvo <faguzman.97@gmail.com>
2025-06-07 08:14:15 -0500

    add optional auth guard

---
+++ src/auth/guards/optionalJwt.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      await super.canActivate(context);
+    } catch {
+      return true;
+    }
+
+    return true;
+  }
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -15,6 +15,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { User } from '../users/entities/user.entity';
 import { GetUser } from '../auth/get-user/get-user.decorator';
 import { QueryProductDto } from './dto/query-product.dto';
+import { OptionalJwtAuthGuard } from '../auth/guards/optionalJwt.guard';
 
 @Controller('products')
 export class ProductsController {
@@ -29,6 +30,7 @@ export class ProductsController {
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @UseGuards(OptionalJwtAuthGuard)
   findAll(@Query() queryDto: QueryProductDto, @GetUser() user?: User) {
     return this.productsService.findAll(queryDto, user);
   }
